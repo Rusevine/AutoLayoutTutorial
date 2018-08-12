32aaf932d112c158597962289d4f3ff3b2cfe828
Wiljay <wiljay.flores@gmail.com>
2018-08-12 18:27:11 -0400

    created gallery view controller

---
+++ AutoLayoutTutorial/GalleryViewController.h
@@ -0,0 +1,13 @@
+//
+//  GalleryViewController.h
+//  AutoLayoutTutorial
+//
+//  Created by Wiljay Flores on 2018-08-12.
+//  Copyright © 2018 wiljay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GalleryViewController : UIViewController
+
+@end
